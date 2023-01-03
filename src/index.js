623b366bec08ca2a8c43dd1aece6ce1114e120ce
chris-wamae <chriswamae123@gmail.com>
2023-01-03 19:22:31 +0300

    Added quotes to the DOM using forEach

--- src/index.js
+++ src/index.js
@@ -6,18 +6,19 @@ function renderQuotes() {
     .then(function (quotesObject) {
       //adds quotes to the page
       console.log(quotesObject);
-      quotesObject.forEach(function(){
+      quotesObject.forEach(function(singleQuote){
         let quote = document.createElement("li")
         quote.setAttribute("class","quote-card")
         quote.innerHTML = 
         `<blockquote class='blockquote'> 
-             <p class='mb-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p> 
-                 <footer class='blockquote-footer'>Someone famous</footer>
+             <p class='mb-0'>${singleQuote["quote"]}</p> 
+                 <footer class='blockquote-footer'>${singleQuote["author"]}</footer>
                  <br> 
                      <button class='btn-success'> Likes: 
-                        <span>0</span> </button>
+                        <span>0</span></button>
                      <button class='btn-danger'>Delete</button>
             </blockquote>`;
+            console.log(singleQuote["likes"])
         document.querySelector("ul#quote-list").append(quote)
 
       })
