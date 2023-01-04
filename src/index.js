51c71f94c0cb09223116ece16bcb64107b6ba9a3
chris-wamae <chriswamae123@gmail.com>
2023-01-04 06:33:49 +0300

    Created a new quote object that takes the stored variables and a fetch request which pushes them to the server

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function renderQuotes() {
                         <span>0</span></button>
                      <button class='btn-danger'>Delete</button>
             </blockquote>`;
-            console.log(singleQuote["likes"])
+            console.log(singleQuote)
         document.querySelector("ul#quote-list").append(quote)
 
       })
@@ -34,7 +34,21 @@ event.preventDefault()
 //get the data that is entered when the form is submitted
 let newQuote = document.querySelector("input#new-quote").value
 let quoteAuthor = document.querySelector("input#author").value
-})
 // Submitting the form creates a new quote and adds it to the list of quotes without 
 // having to refresh the page. Pessimistic rendering is recommended.
-} addQuote()
+//creates a new quote object for the server
+let newQuoteObject = {}
+newQuoteObject["quote"] = newQuote
+newQuoteObject["author"] = quoteAuthor
+newQuoteObject["likes"] = []
+//sends a POST request with the new quote object to the server
+fetch("http://localhost:3000/quotes",{
+  method:"POST",
+  headers:{"Content-Type":"application/json"},
+  body:JSON.stringify(newQuoteObject)
+})
+//renders the quote on the page
+renderQuotes()
+})
+}addQuote()
+
